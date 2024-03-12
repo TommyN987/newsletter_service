edc39fb545e94202a254b2447a0de2c105f85a4a
tommy <tomas.nagy.tn@gmail.com>
2024-03-12 22:47:03 +0100

    feat: add postgres, first migration and query

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 use std::net::TcpListener;
 
-use newsletter_service::run;
+use newsletter_service::{configuration::get_config, startup::run};
+use sqlx::PgPool;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
+    let config = get_config().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&config.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
+    let addr = format!("127.0.0.1:{}", config.app_port);
+    let listener = TcpListener::bind(addr).expect("Failed to bind to random port");
 
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
